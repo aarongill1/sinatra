965cc402f39e6c507811662ee042ec08ed3cbe2c
Aaron Gill <izzydiggler@gmail.com>
2018-11-13 15:33:49 +0000

    added form to get name from user input on page. Also name only appears after it has been set.

--- app.rb
+++ app.rb
@@ -12,7 +12,13 @@ get '/cats' do
   "beware of the cats!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
   erb(:index)
 end
